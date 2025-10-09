cb97b8f976478101fe180014869bb7c16f3b60d6
Tomas Taboada <tomastaboada27@gmail.com>
2025-10-09 17:42:07 -0300

    change gallery distribution && home style

--- Trabajo-Integrador/src/gallery.jsx
+++ Trabajo-Integrador/src/gallery.jsx
@@ -5,7 +5,6 @@ export default function Gallery({pokeData}) {
 
   return (
     <>
-      <h1>GALLERY</h1>
         <div className="container">
           {pokeData.map((pokemon, index) => (
             <PokeCard key={index} pokemonData={pokemon}/>
